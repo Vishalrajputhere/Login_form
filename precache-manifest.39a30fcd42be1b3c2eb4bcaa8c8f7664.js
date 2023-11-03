2865f1f9a05babfa3b24c65b13f604421039fdd6
Vishal Rajput <singhrajputvishal03@gmail.com>
2023-11-03 10:00:46 +0530

    Updates

---
+++ precache-manifest.39a30fcd42be1b3c2eb4bcaa8c8f7664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ceb77c01bd49f81ddaa7f406a2cc619",
+    "url": "/Login_form/index.html"
+  },
+  {
+    "revision": "9e57b83427b9216270b9",
+    "url": "/Login_form/static/css/main.6d1f63da.chunk.css"
+  },
+  {
+    "revision": "a867be105b0afd671cfa",
+    "url": "/Login_form/static/js/2.6e939aa1.chunk.js"
+  },
+  {
+    "revision": "9e57b83427b9216270b9",
+    "url": "/Login_form/static/js/main.28153ec7.chunk.js"
+  },
+  {
+    "revision": "0e701fe6174c91f6aafd",
+    "url": "/Login_form/static/js/runtime~main.6f375d48.js"
+  }
+]);
